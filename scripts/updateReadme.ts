30a603016cbc3a2095e07110857a4565da341c74
Marcus <marcuskristensen10@hotmail.com>
2026-04-23 10:15:59 +0200

    chore: made only programming jokes be able to get used.

--- scripts/updateReadme.ts
+++ scripts/updateReadme.ts
@@ -12,12 +12,9 @@ const JOKE_MARKER = /<!--JOKE-->[\s\S]*?<!--\/JOKE-->/;
 const PUNCHLINE_MARKER = /<!--PUNCH-->[\s\S]*?<!--\/PUNCH-->/;
 
 async function main() {
-  const response = await fetch("https://official-joke-api.appspot.com/jokes/random");
-  if (!response.ok) {
-    throw new Error(`API request failed: ${response.status} ${response.statusText}`);
-  }
+    
+  const data = await GetProgrammingJoke();
 
-  const data = await response.json() as QuoteResponse;
   const joke = data.setup;
   const punch = data.punchline;
 
@@ -41,3 +38,17 @@ main().catch((error) => {
   console.error(error);
   process.exit(1);
 });
+
+
+const GetProgrammingJoke = async () => {
+    while(true){
+        const response = await fetch("https://official-joke-api.appspot.com/jokes/random")
+        if(!response.ok) throw new Error(`API request failed: ${response.status} ${response.statusText}`);
+
+        const data = await response.json() as QuoteResponse;
+
+        if(data.type==="programming") return data;
+
+        console.log("Skipped non-programming joke:", data.type)
+    }
+}
